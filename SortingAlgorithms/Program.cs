ad8306a000f600cdc05762ad18514d097d5479c3
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2021-01-25 11:16:07 +0100

    Added BubbleSort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -6,7 +6,27 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 4, 1, 3, 2 };
+            BubbleSort(array);
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        //Compares the last index with the 1 before it, and swaps them if they are not in the correct order. Repeats this process "n" times.
+        private static void BubbleSort(int[] array)
+        {
+            for(int i = 1; i < array.Length; i++)
+            {
+                for(int j = array.Length -1; j >= i; j--)
+                {
+                    if(array[j] < array[j - 1])
+                    {
+                        (array[j -1], array[j]) = (array[j], array[j-1]);
+                    }
+                }
+            }
         }
     }
 }
